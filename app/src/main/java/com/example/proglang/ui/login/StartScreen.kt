dbe81ac75fc985d2e99a0ee501a4d8559324ac7e
patrickoshea <oshea_patrick@hotmail.com>
2020-03-24 15:20:22 -0400

    SQL Working!!!

--- app/src/main/java/com/example/proglang/ui/login/StartScreen.kt
+++ app/src/main/java/com/example/proglang/ui/login/StartScreen.kt
@@ -1,31 +1,27 @@
 package com.example.proglang.ui.login
 
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.adamratzman.spotify.SpotifyApi
 import com.example.proglang.R
-import com.example.proglang.Songs.Queue
 import com.example.proglang.Songs.Song
 import com.example.proglang.global.globals
 import com.example.proglang.songQueueActivity
-import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
 import com.spotify.protocol.types.PlayerState
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.transactions.transaction
+import java.sql.DriverManager
+import java.sql.PreparedStatement
+import java.sql.ResultSetMetaData
 
 
 class StartScreen : AppCompatActivity() {
-
     // Connection data for API
 
     // Data for Queue
@@ -33,13 +29,115 @@ class StartScreen : AppCompatActivity() {
     private var trackWasStarted = false
     var songOnQueue : Boolean = false
     var spotifyAppRemote = globals.spotifyAppRemote
+    var get = getFromSQL()
+    var post = postToSQL()
 
 
     object Songs : Table() {
-        val URI = varchar("URI", 48)
+        val URI = varchar("URI", 45)
         val user = varchar("user", 45)
         val numVotes = integer("numVotes")
     }
+
+
+    public class getFromSQL :
+        AsyncTask<String?, Void?, String>() {
+        var res = ""
+        val url = "jdbc:mysql://44.229.52.223:3306/data_collection"
+        val user = "test_user"
+        val pass = "proglang"
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+        override fun onPostExecute(result: String) {
+           Log.d("Result Printing", result)
+        }
+        override fun doInBackground(vararg params: String?): String {
+            try {
+                Class.forName("com.mysql.jdbc.Driver")
+                val con = DriverManager.getConnection(url, user, pass)
+                println("Databaseection success")
+                var result = "Database Connection Successful\n"
+                val st = con.createStatement()
+                val rs =
+                    st.executeQuery("select distinct URI from Table1")
+                val rsmd: ResultSetMetaData = rs.metaData
+                while (rs.next()) {
+                    result += """
+                        ${rs.getString(1)}
+                        
+                        """.trimIndent()
+                }
+                res = result
+            } catch (e: Exception) {
+                e.printStackTrace()
+                res = e.toString()
+            }
+            return res
+        }
+    }
+
+
+
+    public class postToSQL :
+        AsyncTask<String?, Void?, String>() {
+        var res = ""
+        val url = "jdbc:mysql://44.229.52.223:3306/data_collection"
+        val user = "test_user"
+        val pass = "proglang"
+
+        var URI : String? = null
+        var usr :String? = null
+        var numVotes : Int = 0
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        fun update(a : String?, b : String? , c : Int) {
+            URI = a;
+            usr = b;
+            numVotes = c;
+        }
+
+        override fun onPostExecute(result: String) {
+            Log.d("Result Printing", result)
+        }
+        override fun doInBackground(vararg param1: String?): String {
+            try {
+                Class.forName("com.mysql.jdbc.Driver")
+                val conn = DriverManager.getConnection(url, user, pass)
+
+                // create a sql date object so we can use it in our INSERT statement
+
+                // the mysql insert statement
+                val query =
+                    (" insert into Table1 (URI, user, numVotes)"
+                            + " values (?, ?, ?)")
+
+                // create the mysql insert preparedstatement
+                val preparedStmt: PreparedStatement = conn.prepareStatement(query)
+                preparedStmt.setString(1, URI)
+                preparedStmt.setString(2, usr)
+                preparedStmt.setInt(3, numVotes)
+
+                // execute the preparedstatement
+                preparedStmt.execute()
+                conn.close()
+            } catch (e: java.lang.Exception) {
+                Log.d("Got an exception!", e.message)
+            }
+            return res
+        }
+    }
+
+
+
+    fun postToSQLServer(uri : String, user : String, numVotes : Int) {
+        post.update(uri, user, numVotes)
+        post.execute("")
+    }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,6 +147,9 @@ class StartScreen : AppCompatActivity() {
         val playButton  = findViewById<Button>(R.id.playButton)
         val textField :EditText = findViewById<EditText>(R.id.SongId)
         val toSongQueueButton =  findViewById<Button>(R.id.toSongQueueButton)
+
+
+        //get.execute("")
 
 
         toSongQueueButton.setOnClickListener{
@@ -67,6 +168,9 @@ class StartScreen : AppCompatActivity() {
             ).show()
 
             var tempSong : Song = Song(textField.text.toString(), "", 1)
+
+            postToSQLServer(textField.text.toString(), "testUser", 1)
+
             songQueue.push(tempSong)
             textField.text.clear()
 
